dc233f6d102b711c4b74af7e793d425a6fd1c4bf
Pooja Singh <Pooja.Singh@gigatorb.com>
2025-04-14 16:23:31 +0530

    push

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ import express from "express";
 import dotenv from "dotenv";
 import { db } from "./models/index.js";
 import { router } from "./routes/index.js";
+import cors from 'cors'
 import bodyParser from 'body-parser';
 // import {createAdmin} from './controller/admin.js'
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
 
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "RefreshToken"],
+  exposedHeaders: ["Content-Length", "Content-Type", "RefreshToken", "Token"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// app.options("*", cors(corsOptions));
+
 
 app.use("/api", router);
 
